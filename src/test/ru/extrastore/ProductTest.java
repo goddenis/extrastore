a804ae9493303b27a7aacc33f0e15a69093507e3
stas.versilov <stas.versilov@d51e8d21-4554-4d40-6ef2-7fe45455bcdc>
2011-02-13 08:56:19 +0000

    TestNG configuration and simple ProductTest.

---
+++ src/test/ru/extrastore/ProductTest.java
@@ -0,0 +1,24 @@
+package ru.extrastore;
+
+import org.jboss.seam.mock.SeamTest;
+import org.testng.annotations.Test;
+import ru.extrastore.model.Product;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+/**
+ * File profile
+ * Creator: Catalyst
+ * Date: 13.02.11
+ * Time: 11:17
+ */
+public class ProductTest extends SeamTest {
+    private static final String PRODUCT_NAME = "Тестовый Продукт";
+
+    @Test
+    public void testProduct() {
+        Product product = new Product();
+        product.setName(PRODUCT_NAME);
+        assertEquals("Product name", PRODUCT_NAME, product.getName());
+    }
+}
