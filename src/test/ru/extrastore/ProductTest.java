9e19d15121c0771c326c7d23060ff7d77cd1adb4
stas.versilov <stas.versilov@d51e8d21-4554-4d40-6ef2-7fe45455bcdc>
2011-02-13 10:06:44 +0000

    TestNG configuration and simple ProductTest. Hard bug with NoSuchMethodException in tests understood and removed.

--- src/test/ru/extrastore/ProductTest.java
+++ src/test/ru/extrastore/ProductTest.java
@@ -16,7 +16,7 @@ public class ProductTest extends SeamTest {
     private static final String PRODUCT_NAME = "Тестовый Продукт";
 
     @Test
-    public void testProduct() {
+    public void testProduct() throws Exception {
         Product product = new Product();
         product.setName(PRODUCT_NAME);
         assertEquals("Product name", PRODUCT_NAME, product.getName());
