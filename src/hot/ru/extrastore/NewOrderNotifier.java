25f8de8f2428256a36acc6604534c0b9673626ff
stas.versilov@gmail.com <stas.versilov@gmail.com@d51e8d21-4554-4d40-6ef2-7fe45455bcdc>
2011-02-22 19:09:04 +0000

    Conversation is prolongated, to display an order number to the customer. Email notification is send now asynchronously, to separate component.

---
+++ src/hot/ru/extrastore/NewOrderNotifier.java
@@ -0,0 +1,36 @@
+package ru.extrastore;
+
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Observer;
+import org.jboss.seam.contexts.Contexts;
+import org.jboss.seam.faces.Renderer;
+import org.jboss.seam.log.Log;
+import ru.extrastore.model.Order;
+
+import java.io.Serializable;
+
+/**
+ * http://extrastore.ru
+ * Created by Ксюха on 22.02.11 at 21:39
+ */
+@Name("newOrderNotifier")
+public class NewOrderNotifier implements Serializable {
+    @Logger
+    Log log;
+
+    @In
+    Renderer renderer;
+
+    @Observer("newOrder")
+    public void onNewOrder(Order newOrder) {
+        log.info("#0.onNewOrder()", this.hashCode());
+        Contexts.getEventContext().set("currentOrder", newOrder);
+        try {
+            renderer.render("/order/email_notification.xhtml");
+        } catch (Exception e) {
+            log.error("Error saving new order", e);
+        }
+    }
+}
