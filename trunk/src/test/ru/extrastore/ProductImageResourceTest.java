73891d71ab978d0b898e601749eccc472850bc48
stas.versilov <stas.versilov@d51e8d21-4554-4d40-6ef2-7fe45455bcdc>
2011-02-27 15:25:34 +0000

    Image loading from database.

---
+++ trunk/src/test/ru/extrastore/ProductImageResourceTest.java
@@ -0,0 +1,29 @@
+package ru.extrastore;
+
+import org.jboss.seam.mock.EnhancedMockHttpServletResponse;
+import org.jboss.seam.mock.ResourceRequestEnvironment;
+import org.jboss.seam.mock.ResourceRequestEnvironment.Method;
+import org.jboss.seam.mock.ResourceRequestEnvironment.ResourceRequest;
+import org.jboss.seam.mock.SeamTest;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+/**
+ * http://extrastore.ru
+ * Created by Catalyst on 27.02.11 at 16:58
+ */
+public class ProductImageResourceTest extends SeamTest {
+
+    static final String IMAGE_PATH = "/r/productImage/noproduct-s.jpg";
+
+    @Test
+    public void testGetProductImage() throws Exception {
+        new ResourceRequest(new ResourceRequestEnvironment(this), Method.GET, IMAGE_PATH) {
+            @Override
+            protected void onResponse(EnhancedMockHttpServletResponse response) {
+                assertEquals("image not found", 404, response.getStatus());
+            }
+        }.run();
+    }
+}
