bd924ad6245a26be34ad4777c51f2195e1b5989e
stas.versilov <stas.versilov@d51e8d21-4554-4d40-6ef2-7fe45455bcdc>
2011-03-07 12:23:34 +0000

    Minor fixes.

--- trunk/src/main/ru/extrastore/model/delivery/DeliveryTypePostal.java
+++ trunk/src/main/ru/extrastore/model/delivery/DeliveryTypePostal.java
@@ -1,5 +1,7 @@
 package ru.extrastore.model.delivery;
 
+import ru.extrastore.model.Order;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
@@ -10,4 +12,12 @@ import javax.persistence.Entity;
 @Entity
 @DiscriminatorValue("POSTAL")
 public class DeliveryTypePostal extends DeliveryType {
+    @Override
+    public long getCost(Order order) {
+        if (this.getFreeAfter() != 0) {
+            return super.getCost(order);    // Fixed delivery cost with free limit
+        } else {
+            return -1; // Calculate cost based on russian post rules.
+        }
+    }
 }
