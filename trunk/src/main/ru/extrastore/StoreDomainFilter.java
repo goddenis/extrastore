a06cf868b05ca38feaf4a0a7b1dd8847e891f22d
stas.versilov <stas.versilov@d51e8d21-4554-4d40-6ef2-7fe45455bcdc>
2011-03-07 13:21:57 +0000

    Filter redirect for host names with "www.". "www." is now stripped.

--- trunk/src/main/ru/extrastore/StoreDomainFilter.java
+++ trunk/src/main/ru/extrastore/StoreDomainFilter.java
@@ -30,76 +30,100 @@ import static org.jboss.seam.annotations.Install.APPLICATION;
  * Date: 30.01.11
  * Time: 3:38
  */
-    @Startup
-    @Scope(ScopeType.APPLICATION)
-    @Name("storeDomainFilter")
-    @Install(precedence = APPLICATION)
-    @BypassInterceptors
-    @Filter(within="org.jboss.seam.web.ajax4jsfFilter")
-    public class StoreDomainFilter implements javax.servlet.Filter {
-        FilterConfig filterConfig;
+@Startup
+@Scope(ScopeType.APPLICATION)
+@Name("storeDomainFilter")
+@Install(precedence = APPLICATION)
+@BypassInterceptors
+@Filter(within="org.jboss.seam.web.ajax4jsfFilter")
+public class StoreDomainFilter implements javax.servlet.Filter {
 
-        private static final Pattern IPAddressPattern = Pattern.compile("(\\d{1,3}\\.){3}\\d{1,3}");
+    private class BooleanWrapper {  // Enables us to change boolean value from inline class ContextualHttpServletRequest.
+        boolean value;
 
-        private static final boolean isIPAddress(String address) {
-            return IPAddressPattern.matcher(address).matches();
+        private BooleanWrapper(boolean value) {
+            this.value = value;
         }
 
-        public void doFilter(final ServletRequest request,
-                             final ServletResponse response,
-                             FilterChain chain)
-                throws java.io.IOException, javax.servlet.ServletException {
-            if (request instanceof HttpServletRequest) {
-                final HttpServletRequest httpRequest = (HttpServletRequest)request;
-                final HttpServletResponse httpResponse = (HttpServletResponse)response;
-
-//                if (!(httpRequest.getServletPath().endsWith(".seam"))) {
-//                    chain.doFilter(request, response);
-//                    return;
-//                }
-
-                new ContextualHttpServletRequest((HttpServletRequest)request) {
-                    public void process() throws Exception {
-
-                        if (Contexts.getSessionContext().get("store") != null) {
-                            return;
-                        }
-
-                        EntityManager em = (EntityManager) Component.getInstance("entityManager");
-                        String domain = request.getServerName();
-                        if (domain == null || isIPAddress(domain)) {
-                            domain = "localhost";
-                        }
-                        if (domain.startsWith("www.")) {
-                            // Remove "www.", redirect to the short name
-                            domain = domain.substring(domain.indexOf(".")+1, domain.length());
-                        }
-
-                        try {
-
-                            Store store = (Store)em.createQuery("from Store s where s.domain = :domainName").setParameter("domainName", domain).getSingleResult();
-
-                            if (store != null) {
-                                Contexts.getSessionContext().set("store", store);
-                                System.out.println("Store domain set to " + store.getDomain() + ", store(" + store.hashCode() + "), session: " + httpRequest.getSession().getId());
-                            }
-
-                        } catch (NoResultException e) {
-                            throw new IllegalArgumentException("Error: No store found for domain " + domain, e);
-                        }
-                    }
-
-                }.run();
-
-            }
-
-            chain.doFilter(request,response);
+        public boolean getValue() {
+            return value;
         }
 
-        public void init(final FilterConfig filterConfig) {
-            this.filterConfig = filterConfig;
-        }
-
-        public void destroy() {
+        public void setValue(boolean value) {
+            this.value = value;
         }
     }
+
+    FilterConfig filterConfig;
+
+    private static final Pattern IPAddressPattern = Pattern.compile("(\\d{1,3}\\.){3}\\d{1,3}");
+
+    private static final boolean isIPAddress(String address) {
+        return IPAddressPattern.matcher(address).matches();
+    }
+
+    public void doFilter(final ServletRequest request,
+                         final ServletResponse response,
+                         final FilterChain chain)
+            throws java.io.IOException, javax.servlet.ServletException {
+
+        final BooleanWrapper continueChain = new BooleanWrapper(true);
+
+        if (request instanceof HttpServletRequest) {
+
+            final HttpServletRequest httpRequest = (HttpServletRequest)request;
+            final HttpServletResponse httpResponse = (HttpServletResponse)response;
+
+
+
+            new ContextualHttpServletRequest((HttpServletRequest)request) {
+                public void process() throws Exception {
+
+                    if (Contexts.getSessionContext().get("store") != null) {
+                        return;
+                    }
+
+                    EntityManager em = (EntityManager) Component.getInstance("entityManager");
+                    String domain = httpRequest.getHeader("Host");
+                    if (domain == null || isIPAddress(domain)) {
+                        domain = "localhost";
+                    }
+                    if (domain.startsWith("www.")) {
+                        // Remove "www.", redirect to the short name
+                        domain = domain.substring(domain.indexOf(".")+1, domain.length());
+                        httpResponse.setStatus(301);
+                        httpResponse.setHeader("Location", "http://" + domain + httpRequest.getRequestURI()
+                                + (httpRequest.getQueryString() != null ? "?" + httpRequest.getQueryString() : ""));
+                        continueChain.setValue(false);
+                        return;
+                    }
+
+                    try {
+
+                        Store store = (Store)em.createQuery("from Store s where s.domain = :domainName").setParameter("domainName", domain).getSingleResult();
+
+                        if (store != null) {
+                            Contexts.getSessionContext().set("store", store);
+                            System.out.println("Store domain set to " + store.getDomain() + ", store(" + store.hashCode() + "), session: " + httpRequest.getSession().getId());
+                        }
+
+                    } catch (NoResultException e) {
+                        throw new IllegalArgumentException("Error: No store found for domain " + domain, e);
+                    }
+                }
+
+            }.run();
+        }
+
+        if (continueChain.getValue()) {
+            chain.doFilter(request,response);
+        }
+    }
+
+    public void init(final FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+    public void destroy() {
+    }
+}
